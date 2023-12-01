db78cd25ec9c051f8d7263214566aa630c28f857
藤橋祐哉 <>
2023-12-01 18:31:22 +0900

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,19 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    'jest/globals': true,
     node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
-  plugins: ['jest'],
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {},
 }
